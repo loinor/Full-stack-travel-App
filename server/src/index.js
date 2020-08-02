c065b9a8941ef38efe0986c385f9e68d76ae61ff
Viacheslav Guskov <gertsslavik@yandex.ru>
2020-08-02 15:19:00 +0300

    Setup express.js functionality

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const middlewares = require('./middlewares');
+
 const app = express();
 app.use(morgan('common'));
 app.use(helmet());
@@ -15,6 +17,10 @@ app.get('/', (req, res) => {
     message: 'Hello, world!',
   });
 });
+
+app.use(middlewares.notFound);
+
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
